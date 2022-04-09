7328d237b42449b1a6a1e40d5fef6f755f03e2c9
Bence <gutanbence@gmail.com>
2022-04-09 12:24:15 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,168 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyElevator
+    v-for="elevator in elevators"
+    :key="elevator.id"
+    :elevator="elevator"
+    :nrOfFloors="nrOfFloors"
+  />
+  <div class="btns">
+    <div v-for="upDwnBtn in upDwnBtns" :key="upDwnBtn.id" class="up-down-btns">
+      <div>{{upDwnBtn.id}}</div>
+      <button @click="btnPress">↑</button>
+      <div>{{upDwnBtn.isPressed}}</div>
+      <button @click="btnPress">↓</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyElevator from "./components/MyElevator.vue";
+import { defineComponent, ref } from "vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
   components: {
-    HelloWorld
-  }
-}
+    MyElevator,
+  },
+  setup() {
+    const nrOfFloors = ref(7);
+    const upDwnBtns = ref([]);
+    for ( let i = 0; i < nrOfFloors.value; i++ ) {
+      upDwnBtns.value.push({isPressed:false,id:i})
+    }
+    console.log(upDwnBtns.value);
+    const elevators = ref([
+      {
+        currentFloor: 0,
+        isMoving: false,
+      },
+      {
+        currentFloor: nrOfFloors.value - 1,
+        isMoving: false,
+      },
+    ]);
+    function btnPress (){
+      console.log(upDwnBtns.value)
+    }
+    return {
+      btnPress,
+      elevators,
+      nrOfFloors,
+      upDwnBtns,
+    };
+  },
+});
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+}
+button {
+  cursor: pointer;
+}
+button:disabled {
+  cursor: auto;
+}
+pre {
+  position: absolute;
+  top: 6rem;
+  left: 34px;
+}
+.pre-f {
+  left: 200px;
+}
+.floor {
+  border: 1px solid #b2b2b2;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  place-content: center;
+  max-width: 34rem;
+  margin: 0 auto;
+  height: 15rem;
+  position: relative;
+}
+
+.passing.floor::after {
+  content: "";
+  position: absolute;
+  top: 20px;
+  left: 50px;
+  width: 20px;
+  height: 20px;
+}
+
+.doors {
+  height: 11rem;
+  border: 1px solid;
+  width: 20rem;
+  margin: 0 auto;
+  position: inherit;
+  top: 10%;
+  left: 15%;
+  display: flex;
+  justify-content: space-between;
+
+  transition: all 1s ease-in-out;
+}
+
+.door {
+  background-color: rgb(255, 199, 199);
+  z-index: 2;
+  transition: width 1s ease-in-out;
+}
+.door:nth-child(1),
+.door:nth-child(2) {
+  height: 10.9rem;
+  border: 1px solid;
+  width: 10rem;
+}
+
+.open .door:nth-child(1) {
+  width: 1rem;
+}
+.open .door:nth-child(2) {
+  width: 1rem;
+}
+
+.btns {
+  justify-content: flex-start;
+  display: flex;
+  align-items: center;
+}
+
+.numered-btns {
+  opacity: 1;
+  margin: 1rem 1rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.numered-btns.appear {
+  opacity: 1;
+}
+
+.elevator-buttons {
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.number {
+  border-radius: 50%;
+  border: 1px solid;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 1.2rem;
+  height: 1.2rem;
+}
+
+.up-down-btns {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/MyElevator.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="floor" v-for="floor in floors" :key="floor.id">
+      <div class="doors">
+        <div class="door"></div>
+        <div class="door"></div>
+        <div class="elevator-buttons">
+          <div class="numered-btns">
+            <button class="number">123</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  props: {
+    elevator: {
+      type: Object,
+      required: true,
+    },
+    nrOfFloors: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const floors = ref([]);
+    for (let i = 0; i < props.nrOfFloors; i++) {
+      floors.value.push({ isHere: i === props.elevator.currentFloor, isPassing: false });
+    }
+    console.log(props.elevator);
+    console.log(floors.value)
+    return {
+      floors,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
